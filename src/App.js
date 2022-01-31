8bffef5cb3f1b39d68fb8702478f664c90f90da4
nabin <nkbhaie2017@gmail.com>
2022-01-30 22:10:19 -0700

    add App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 // import './App.css';
 import Clicker from "./Clicker";
+import ButtonGroup from "./ButtonGroup";
+
+// function App() {
+//   return (
+//     <div className="App">
+//         <Clicker />
+//     </div>
+//   );
+// }
 
 function App() {
-  return (
-    <div className="App">
-        <Clicker />
-    </div>
-  );
-}
+    return (
+      <div className="App">
+          <ButtonGroup />
+      </div>
+    );
+  }
 
 export default App;
